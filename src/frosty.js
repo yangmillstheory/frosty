6c66d4e5cdb0733da2701feee683a79a09d0faad
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-23 23:35:09 -0700

    Fix formatting; use closure

--- src/frosty.js
+++ src/frosty.js
@@ -1,31 +1,30 @@
-let immutable_descriptor_set = property => {
-  let privatized = Symbol(property);
-  let UNINITIALIZED = undefined;
-
-  return {
-    [privatized]: {
-    value: UNINITIALIZED,
-      writable: true
-  },
-
-  [property]: {
-    enumerable: true,
-      get() {
-      return this[privatized];
-    },
-    set(value) {
-      if (this[privatized] !== UNINITIALIZED) {
-        throw new Error(`${property} is immutable`)
-      } else if (value === UNINITIALIZED) {
-        throw new Error(`${property} should be defined`)
-      }
-      this[privatized] = value
-    }
-  }
-}
-};
-
 let freeze = (obj, ...properties) => {
+  let immutable_descriptor_set = property => {
+    let privatized = Symbol(property);
+    let UNINITIALIZED = undefined;
+
+    return {
+      [privatized]: {
+        value: UNINITIALIZED,
+        writable: true
+      },
+
+      [property]: {
+        enumerable: true,
+        get() {
+          return this[privatized];
+        },
+        set(value) {
+          if (this[privatized] !== UNINITIALIZED) {
+            throw new Error(`${property} is immutable`)
+          } else if (value === UNINITIALIZED) {
+            throw new Error(`${property} should be defined`)
+          }
+          this[privatized] = value
+        }
+      }
+    }
+  };
   if (typeof obj !== 'object') {
     throw new Error(`Expected obj ${obj} to be an object`)
   }
